a7baed3b4c48101f7a7cd360d54da29c4b731a71
Victor Invernon Garcia <145662511+1vaitor10@users.noreply.github.com>
2024-02-27 13:48:50 +0100

    a

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-weight-bold text-xl text-white dark:text-gray-800 leading-tight bg-dark p-4">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <div class="py-5 bg-light">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card border-dark">
+                        <div class="card-header bg-dark text-white">{{ __('You\'re logged in!') }}</div>
+
+                        <div class="card-body">
+
+                            <!-- Menú con 4 botones -->
+                            <div class="text-center mb-4">
+                                <a href="{{ url('admin/incidencies') }}" class="btn btn-outline-primary mr-2">Botón 1</a>
+                                <button type="button" class="btn btn-outline-success mr-2">Botón 2</button>
+                                <button type="button" class="btn btn-outline-warning mr-2">Botón 3</button>
+                                <button type="button" class="btn btn-outline-danger">Botón 4</button>
+                            </div>
+
+                            <!-- Contenido adicional -->
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/admin/incidencies');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
